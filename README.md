3cda375b33157a166c678ccd52b649474a57cf6f
Ashish Patel <ashishsushilpatel@gmail.com>
2020-12-26 11:42:27 +0530

    feat: add readme genrator

--- README.md
+++ README.md
@@ -9,3 +9,42 @@
 </p>
 
 <p align="center">📦 about project </p>
+
+<p align="center">
+  <img src="./assets/todos.png" alt="drawing"/>
+</p>
+
+## Todos
+
+<!-- todos starts -->
+- Update readme template
+- Add github action for todos repo
+- Update readmes with social links
+- Fix laptop bag
+- Setup alfred workflows
+- Solve leetcode 606
+- Read “the old man and the sea”
+- Create topic wise list of important leetcode questions
+- Oreilly reactive spring boot chapter 5
+- Create a cli for common tasks
+<!-- todos ends -->
+
+## Last 15 completed todos
+
+<!-- completed starts -->
+* Clean up personal bot - *Dec 26 2020*
+* Get clothes from laundry - *Dec 26 2020*
+* Order chaipatti, ginger, butter - *Dec 18 2020*
+* Get water can - *Dec 18 2020*
+* Do laundry - *Dec 13 2020*
+* Order eggs - *Dec 26 2020*
+* Rewrite the process api using next.js - *Dec 26 2020*
+* Solve leetcode 653 - *Dec 11 2020*
+* Buy plate, cup, strainer - *Dec 07 2020*
+* Print aadhar card - *Dec 13 2020*
+* Get esim - *Dec 13 2020*
+* Add defer option for tasks - *Dec 26 2020*
+* Order from bigbasket - *Dec 06 2020*
+* Spring boot chapter 5 - *Oct 25 2020*
+* Spring boot chapter 4 - *Oct 24 2020*
+<!-- completed ends -->
---
+++ build_readme.py
@@ -0,0 +1,89 @@
+import httpx
+import json
+import pathlib
+import re
+import os
+from datetime import datetime
+
+root = pathlib.Path(__file__).parent.resolve()
+TOKEN = os.environ['TODOIST_TOKEN']
+
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(
+        marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def replace_chunk_no_space(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->`{}`<!-- {} ends -->".format(
+        marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def fetch_todos():
+    url = "https://api.prod.ashish.me/todos".format(
+        TOKEN)
+    todos = httpx.get(url).json()
+
+    tasks = []
+
+    for todo in todos:
+        if not todo['completed']:
+          tasks.append(todo)
+
+    return tasks[::-1]
+
+def fetch_completed():
+    url = "https://api.prod.ashish.me/todos".format(
+        TOKEN)
+    todos = httpx.get(url).json()
+
+    tasks = []
+
+    for todo in todos:
+        if todo['completed']:
+            tasks.append(todo)
+
+    return tasks[::-1]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    readme_contents = readme.open().read()
+
+    todos = fetch_todos()
+    todos_md = "\n".join(
+        [
+            "- {title}".format(
+                title=todo["content"].strip().capitalize(),
+            )
+            for todo in todos
+        ]
+    )
+    rewritten = replace_chunk(readme_contents, "todos", todos_md)
+
+    completed = fetch_completed()[:15]
+    completed_md = "\n".join(
+        [
+            "* {title} - *{date}*".format(
+                title=todo["content"].strip().capitalize(),
+                date=datetime.fromisoformat(todo["completedDate"].split("T")[0]).strftime("%b %d %Y")
+            )
+            for todo in completed
+        ]
+    )
+    rewritten = replace_chunk(rewritten, "completed", completed_md)
+    readme.open("w").write(rewritten)
